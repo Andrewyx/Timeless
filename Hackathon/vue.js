8b2b536b9d8b712503b3cda6cb3f6133fa10a52f
TadjellTheAlchemist <76237371+tadjelllol@users.noreply.github.com>
2023-01-28 20:17:10 -0800

    base test 3
    -timer goes down
    
    -clicker

--- Hackathon/vue.js
+++ Hackathon/vue.js
@@ -1,35 +1,43 @@
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let [milliseconds,seconds,minutes,hours] = [0,10,0,0]; // starting time is 10 seconds
 let timerRef = document.querySelector('#timerDisplay');
 let int = null;
+let timerStarted = false;
+
 
 document.getElementById('startTimer').addEventListener('click', ()=>{
     if(int!==null){
         clearInterval(int);
     }
     int = setInterval(displayTimer,10);
+    timerStarted = true;
 });
-document.getElementById('pauseTimer').addEventListener('click', ()=>{
-    clearInterval(int);
-});
+
 document.getElementById('resetTimer').addEventListener('click', ()=>{
     clearInterval(int);
-    [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-    timerRef.innerHTML = '00 : 00 : 00 : 000 ';
+    [milliseconds,seconds,minutes,hours] = [0,10,0,0];
+    timerRef.innerHTML = '00 : 10 : 00 : 000 ';
+    score = 0;
+    
 });
 
 function displayTimer(){
-    milliseconds+=10;
-    if(milliseconds >= 1000){
-        milliseconds = 0;
-        seconds++;
-        if(seconds == 60){
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){
-                minutes = 0;
-                hours++;
+    milliseconds-=10;
+    if(milliseconds < 0){
+        milliseconds = 990;
+        seconds--;
+        if(seconds < 0){
+            seconds = 59;
+            minutes--;
+            if(minutes < 0){
+                minutes = 59;
+                hours--;
             }
         }
+    }
+    if(hours == 0 && minutes == 0 && seconds == 0 && milliseconds == 0) {
+        clearInterval(int);
+        timerRef.innerHTML = 'Time\'s up!';
+        return;
     }
     let h = hours < 10 ? "0" + hours : hours;
     let m = minutes < 10 ? "0" + minutes : minutes;
@@ -37,3 +45,7 @@ function displayTimer(){
     let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
     timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
 }
+
+
+
+
